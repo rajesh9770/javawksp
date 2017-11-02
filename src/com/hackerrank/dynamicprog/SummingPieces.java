bd0c97330c39b90dd3aa185a8cbd2e4103a1237d
Rajesh <rajesh.manjrekar@dell.com>
2017-11-01 22:09:20 -0700

    summing pieces 2^n formula series

--- src/com/hackerrank/dynamicprog/SummingPieces.java
+++ src/com/hackerrank/dynamicprog/SummingPieces.java
@@ -7,30 +7,77 @@ import java.util.Scanner;
 /**
  * Created by Rajesh on 10/28/2017.
  */
+
+/**
+ *
+ * In array of length i, # of contiguous splits are 2^(i-1) for i >=1 and 1 for i = 0.
+ *
+ * S0= (2^n) -1 = n*1 + (n-1)*1 + (n-2) * 2 + (n-3) * (2^2) + ...+ 3* 2^(n-4)+ 2 * 2^(n-3) + 1*2^(n-2)
+ * RHS: Consider an array of size n. Consider a[0] and start counting the number of splits such that a[0] is contained in n size piece, n-1 size piece, ... 1 size piece.
+ *
+ * Now consider a[1] and start counting the number of splits such that a[1] is contained in n size piece, n-1 size piece, ... 1 size piece.
+ * S1= n*1 + (n-1)*[1+1] + (n-2)*[1+2^1] + (n-3)*[2^1+2^2] + ... + 3*[2^(n-5) + 2^(n-4)] + 2*[2^(n-4) + 2*(n-3)] + 1*2^(n-3)
+ * See that S1 - S0 = (n-1)*1 + (n-2) *1 + (n-3) *2 + ... + 3* 2^(n-5) + 2* 2^(n-4) - 1 * 2^(n-3)
+ *                  = (n-1)*1 + (n-2) *1 + (n-3) *2 + ... + 3* 2^(n-5) + 2* 2^(n-4) + 1 * 2^(n-3) - 1 * 2^(n-3) - 1 * 2^(n-3)
+ *                  = [(n-1)*1 + (n-2) *1 + (n-3) *2 + ... + 3* 2^(n-5) + 2* 2^(n-4) + 1 * 2^(n-3)] - 2^(n-2)
+ *                  = [2^(n-1) -1] - 2^(n-2)   ## the term inside [] is S0 with n replaced by n-1.
+ *                  = 2^(n-2) -1
+ *
+ *
+ * Now consider a[2] and start counting the number of splits such that a[2] is contained in n size piece, n-1 size piece, ... 1 size piece.
+ * S2= n*1 + (n-1)*[1+1] + (n-2) * [2+1+2] + (n-3)* [2*1 + 1*2 + 1*2^2] + (n-4) * [2*2 + 1*2^2 + 1*2^3] + (n-5) *[2*2^2 + 1*2^3 + 1*2^4] + ...
+ *              3*[2*2^(n-6) + 1*2^(n-5) + 1*2^(n-4)] + 2 *[2*2^(n-5) + 1*2^(n-4)] + 1 * [2^1 * 2^(n-4)]
+ * S2-S1 =  (n-2) * 2 + [(n-3) *2 + (n-4) * 2^2 + (n-5) * 2^3 + .... +3* 2^(n-5)]      - 2 * 2^(n-4).
+ *       =  (n-2) *2  + [ 2^(n-1) -1  - {(n-1) + (n-2) + 2 * 2^(n-4) + 1 * 2^(n-3) } ] - 2*2^(n-4)
+ *       =  2^(n-1) -2 - 3 * 2 ^(n-3)
+ *       = 2^(n-3) (2^2-3) -2
+ *       = 2^(n-3) - 2.
+ */
+
 public class SummingPieces {
 
     private static final int MOD = 1000000007;
     private long [] partitions; //#of ways to split the array of size n into contiguous pieces
     private int n;
     private long[] windowSizeTimesNumOfPieces;
+    private long [] twoPowers;
 
-    public SummingPieces(int n){
+    public SummingPieces(int n, boolean optimize){
         this.n = n;
         partitions = new long[n+1];
-        partitions[0] =1;
-        partitions[1] =1;
+        twoPowers = new long[n+1];
+        partitions[0] = 1;
+        partitions[1] = twoPowers[0] = 1;
         for(int i=2; i<=n; ++i){
             partitions[i] = partitions[i-1] *2; //2^(i-1) - There are i-1 gaps between i numbers; you can pick or do not pick them.
             partitions[i] %= MOD;
+            twoPowers[i-1] = partitions[i];
         }
+        twoPowers[n] = (partitions[n] * 2) % MOD;
         windowSizeTimesNumOfPieces = (n%2) == 0 ? new long[n/2] : new long [(n/2)+1];
+        if(!optimize) method1();
+        else optimizeMethod();
+    }
+
+    private void optimizeMethod() {
+        windowSizeTimesNumOfPieces[0] = twoPowers[n] - 1  ;  // 2^(n)-1
+        for(int index=1; index<windowSizeTimesNumOfPieces.length; ++index){
+            // window[i] = window[i-1] + 2^(n-1-i) - 2 ^(i-1)
+            windowSizeTimesNumOfPieces[index] = (windowSizeTimesNumOfPieces[index-1] + twoPowers[n-1-index]) % MOD;
+            windowSizeTimesNumOfPieces[index] -= twoPowers[index-1];
+            windowSizeTimesNumOfPieces[index] = (windowSizeTimesNumOfPieces[index] + MOD) % MOD;
+            //System.out.println("index: " + index + " " + windowSizeTimesNumOfPieces[index]);
+        }
+    }
+
+    public void method1(){
         for(int index=0; index<windowSizeTimesNumOfPieces.length; ++index){
             windowSizeTimesNumOfPieces[index] = 0;
             for(int pieceSize=1; pieceSize<=n; ++pieceSize){
                 windowSizeTimesNumOfPieces[index] += (window(index, pieceSize) * pieceSize) % MOD;
                 windowSizeTimesNumOfPieces[index] %= MOD;
             }
-            System.out.println("index: " + index + " " + windowSizeTimesNumOfPieces[index]);
+            //System.out.println("index: " + index + " " + windowSizeTimesNumOfPieces[index]);
         }
     }
 
@@ -65,7 +112,7 @@ public class SummingPieces {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        SummingPieces sp = new SummingPieces(n);
+        SummingPieces sp = new SummingPieces(n, true);
         long ret =0;
         for(int i = 0; i < n; i++){
             int val = in.nextInt();
